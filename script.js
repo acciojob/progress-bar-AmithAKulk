25cdf9a357cc9e73381a74f0a16b27d90cb914a6
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-28 19:26:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+const dash = document.getElementsByClassName('dash');
+const circle = document.getElementsByClassName('circle');
+
+prev.addEventListener('click' ,() => {
+    
+    for(let i = dash.length-1 ; i >= 0; i--){
+        if(dash[i].className.includes('active')){
+            dash[i].classList.remove('active');
+            break;
+        }
+    }
+    for(let i = circle.length-1 ; i >= 0; i--){
+        if(circle[i].className.includes('active')){
+            circle[i].classList.remove('active');
+            break;
+        }
+    }
+})
+
+next.addEventListener('click' ,() => {
+	prev.disabled = false;
+    for(let i = 0 ; i <= dash.length-1; i++){
+        if(!dash[i].className.includes('active')){
+            dash[i].classList.add('active');
+            break;
+        }
+    }
+    for(let i = 0 ; i < circle.length; i++){
+        if(!circle[i].className.includes('active')){
+            circle[i].classList.add('active');
+			if(circle[i] == circle[circle.length-1]){
+                next.disabled = true;
+            }
+            else{
+                next.disabled = false;
+            }
+            break;
+        }
+    }  
+})
